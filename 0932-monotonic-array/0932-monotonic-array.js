a8e4a53e1a785e5dac45616d0033fb88720456f9
Safwan Ahmad <123302765+mrsaffu@users.noreply.github.com>
2025-06-23 23:58:15 +0530

    Time: 3 ms (47.41%), Space: 66.8 MB (71.01%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.js
+++ 0932-monotonic-array/0932-monotonic-array.js
@@ -3,29 +3,16 @@
  * @return {boolean}
  */
 var isMonotonic = function (nums) {
-    if(nums.length ===0){
-        return true 
-    } 	
-    const first = nums[0];
-    const last = nums[nums.length-1];
-// 1.......10
-    if(first === last){
-        for(let i=0;i<nums.length-1;i++){
-            if(nums[i+1]!==nums[i]) return false;
-        }
-    }
-    else if (first<last){
-        // non decreasing
-        for(let i=0;i<nums.length-1;i++){
-            if(nums[i+1]<nums[i]) return false;
-        }
-    }
-    else{
-        // non increasing
-        for(let i=0;i<nums.length-1;i++){
-            if(nums[i+1]>nums[i]) return false;
-        } 
-    }
-    return true;
+ let isMonotonicInc = true;
+ let isMonotonicDec = true;
 
+ for(let i=1; i<nums.length; i++){
+    if(nums[i]<nums[i-1]){
+        isMonotonicInc = false;
+    }
+    if(nums[i]>nums[i-1]){
+        isMonotonicDec = false;
+    }
+ }
+  return isMonotonicInc || isMonotonicDec
 };
